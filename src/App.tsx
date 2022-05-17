6269c1ee298fb39f6a1f300eae7328abcebb4f92
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-05-17 15:54:25 +0300

    gh pages connected
    + testing content changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 function App() {
   return (
-   <h1 className="text-violet-900">Hi there</h1>
+    <>
+      <h1 className="text-violet-900">Hi there</h1>
+      <p>Laba diena</p>
+    </>
   );
 }
 
