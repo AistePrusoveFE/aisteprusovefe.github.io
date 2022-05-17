8d05e9a4ac330a594cf43769fcdc8e0e651af334
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-05-17 19:35:08 +0300

    style tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-violet-900">Hi there</h1>
+      <div className="w-20 h-20 bg-blue-500 mx-auto my-6"></div>
       <p>Laba diena</p>
     </>
   );
