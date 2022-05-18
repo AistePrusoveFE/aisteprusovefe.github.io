d8db8c15ab309046a603c1c30eae95f1551dc58d
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-05-18 15:00:47 +0300

    hook fixed, and responsive menu started

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import logo from "../assets/img/ap_logo_2.png"
 import { RiMenu5Fill } from "react-icons/ri"
 import { useState } from "react"
+import useMediaQuery from "../hooks/useMediaQuery"
 
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false)
+  const isMedium = useMediaQuery('(min-width: 768px)')
 
   const showMenuHandler = () => {
-    console.log(showMenu)
     setShowMenu(!showMenu)
   }
 
@@ -14,7 +15,7 @@ export default function Navbar() {
     <div className="pt-4 pb-8">
       <nav className="h-16 py-2 px-4 flex justify-between items-center">
         <img src={logo} alt="" className="h-full"/>
-        <div className="cursor-pointer p-2 hover:bg-red-200 rounded-full relative z-20" onClick={showMenuHandler}>
+        <div className={isMedium ? 'hidden' : 'cursor-pointer p-2 hover:bg-red-200 rounded-full relative z-20'} onClick={showMenuHandler}>
             <RiMenu5Fill className="text-2xl"/>
         </div>
         <div className={showMenu ? 'visible fixed inset-0 w-screen h-screen bg-black opacity-70 z-10' : 'hidden'}></div>
