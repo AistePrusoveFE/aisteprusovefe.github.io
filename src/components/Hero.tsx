42eedcdc41b06ab9568a44f5bba9155de7d48a3f
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-05-20 17:05:23 +0300

    logo changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <Wrapper>
             <h1 className="font-medium text-2xl mb-4 apolline">Hello there! I am <span className="text-4xl block title mt-2">Aiste Prusove</span></h1>
             <div className={`${isSmall ? 'max-w-[450px] mx-auto' : ''} text-center`}>
-                <img src={avatar} alt="" className={`mx-auto m-8 border-8 border-red-400 border-dotted rounded-full`} />
+                <img src={avatar} alt="" className={`mx-auto m-8`} />
             </div>
             <div>
                 <p className="apolline mb-40">awesome &amp; perky developer</p>
