b0ca82caca2d68e639292d242eb6c789605bfb7c
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-05-24 12:11:38 +0300

    navbar fonts fixed

--- src/components/navbar/AllNavlinks.tsx
+++ src/components/navbar/AllNavlinks.tsx
@@ -1,14 +1,17 @@
+import useMediaQuery from "../../hooks/useMediaQuery"
 import ToggleTheme from "../ToggleTheme"
 import { CloseMenu } from "./Menu"
 import SingleNavlink from "./SingleNavlink"
 
 const AllNavlinks = ({ onClick, className }: CloseMenu) => {
+    const isMedium = useMediaQuery('(min-width: 769px')
+
     return (
         <div className={`${className}`}>
             <SingleNavlink linkId="works" text="works" onClick={onClick} />
             <SingleNavlink linkId="about" text="about" onClick={onClick}/>
             <SingleNavlink linkId="blog" text="articles" onClick={onClick}/>
-            <a href="https://www.instagram.com/awesome_perky_tech.diaries/" target="_blank" className="fenwick text-pink-400">@awesome_perky_tech</a>
+            <a href="https://www.instagram.com/awesome_perky_tech.diaries/" target="_blank" className={`${isMedium ? 'interstate' : 'fenwick-outline'} text-pink-400`}>@awesome_perky_tech</a>
             <ToggleTheme />
         </div>
     )
--- src/components/navbar/SingleNavlink.tsx
+++ src/components/navbar/SingleNavlink.tsx
@@ -11,7 +11,7 @@ interface NavlinkProps {
 const SingleNavlink = ({ text, linkId, onClick }: NavlinkProps) => {
     const isMedium = useMediaQuery('(min-width: 769px')
     return (
-        <a href={`#${linkId}`} onClick={onClick} className={`fenwick-outline`}>{text}</a>
+        <a href={`#${linkId}`} onClick={onClick} className={`${isMedium ? 'interstate' : 'fenwick-outline'}`}>{text}</a>
     )
 }
 
