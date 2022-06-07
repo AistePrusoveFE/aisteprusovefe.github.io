cff4bf54de4ab316833e7863477b3998479eefd2
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-06-07 15:13:33 +0300

    tag btn style update

--- src/ui/TagBtn.tsx
+++ src/ui/TagBtn.tsx
@@ -20,7 +20,7 @@ const TagBtn = ({ catItem, onActivate, onDeactivate, active }: SingleTagProps) =
     return (
         <button
             onClick={onActiveToggle}
-            className={`${active ? 'bg-[#ff0088] border-[#ff0088]' : 'bg-zinc-900'} inline-block px-4 text-xs w-fit py-1 m-1 rounded-full border-2 border-zinc-900 text-zinc-50 hover:border-[#ff0088]`}>
+            className={`${active ? 'bg-[#ff0088] border-[#ff0088]' : 'bg-zinc-900 '} inline-block px-4 text-xs w-fit py-1 m-1 rounded-full border-2 border-zinc-900 text-zinc-50 hover:border-[#ff0088] dark:border-zinc-600`}>
             {catItem}
         </button>
     )
