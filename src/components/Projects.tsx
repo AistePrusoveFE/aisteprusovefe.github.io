ac3d50154a1860e6cf5bea15dd7f9167d92ce598
Aistė <97667702+AistePrusoveFE@users.noreply.github.com>
2022-06-07 09:20:07 +0300

    refactoring filter functionality

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import data, { CategoryType, SingleProjectProps } from "../content";
+import data, { SingleProjectProps } from "../content";
+import { CategoryType } from "../content"
 import SectionTitle from "../ui/SectionTitle";
 import Tag from "../ui/Tag";
 import Wrapper from "../ui/Wrapper";
@@ -7,66 +8,26 @@ import SingleProject from "./SingleProject";
 
 const Projects = () => {
     const [projects, setProjects] = useState(data.projects)
-    const [filteredCat, setFilteredCat] = useState('')
 
-    const onFilterCategory = (category: CategoryType) => {
-        if (!filteredCat) {
-            let foundProjects: SingleProjectProps[] = []
-            projects.forEach(project => {
-                if (project.category.includes(category)) {
-                    foundProjects.push(project)
-                }
-            })
+    const onFilterHandler = (tag: CategoryType) => {
+        let filteredPrjs: SingleProjectProps[] = [];
 
-            setProjects(foundProjects)
-            setFilteredCat(category)
-        }
-
-        if (filteredCat) {
-            projects.forEach(project => {
-                if (project.category.every(prjCat => prjCat === category)) {
-
-                    setProjects(data.projects)
-                    setFilteredCat('')
-                }
-
-                if (!project.category.includes(category)) {
-                    let foundProjects: SingleProjectProps[] = []
-                    data.projects.forEach(project => {
-                        if (project.category.includes(category)) {
-                            foundProjects.push(project)
-                        }
-                    })
-
-                    setProjects(foundProjects)
-                    setFilteredCat(category)
-                }
-            })
-        }
+        projects.forEach(prj => {
+            if (prj.category.includes(tag)){
+                filteredPrjs.push(prj)
+            }
+        })
+        
+        setProjects(filteredPrjs)
     }
 
     return (
         <Wrapper className="px-4">
             <SectionTitle text="Check My Works" />
             <div className="py-4 text-center">
-                <Tag 
-                    filteredCat={filteredCat}
-                    category="web" 
-                    text="web development" 
-                    onClick={() => onFilterCategory('web')} 
-                />
-                <Tag 
-                    filteredCat={filteredCat}
-                    category="design" 
-                    text="design &amp; branding" 
-                    onClick={() => onFilterCategory('design')} 
-                />
-                <Tag 
-                    filteredCat={filteredCat}
-                    category="art" 
-                    text="illustrations" 
-                    onClick={() => onFilterCategory('art')} 
-                />
+                <Tag text="web development" onClick={() => onFilterHandler('web')}/>
+                <Tag text="design &amp; branding" onClick={() => onFilterHandler('design')}/>
+                <Tag text="illustrations" onClick={() => onFilterHandler('art')}/>
             </div>
             <div id="works" className="mt-8 grid gap-y-4 md:gap-12 md:grid-cols-2 lg:grid-cols-3">
                 {projects.map((project, index) => {
--- src/ui/Tag.tsx
+++ src/ui/Tag.tsx
@@ -3,16 +3,18 @@ import { useState } from "react"
 interface TagProps {
     text: string
     onClick: () => void
-    category: string
-    filteredCat: string
 }
 
-const Tag = ({ text, onClick, category, filteredCat }: TagProps) => {
+const Tag = ({ text, onClick }: TagProps) => {
+    const [filterOn, setFilterOn] = useState(false);
+    const onFilterToggle = () => {
+        setFilterOn(!filterOn)
+    }
 
     return (
         <button
-            onClick={onClick}
-            className={`${filteredCat === category ? 'bg-[#ff0088]' : 'bg-zinc-900'} inline-block px-4 text-xs w-fit py-1 m-1 rounded-full border-2 border-zinc-900  text-zinc-50 hover:border-[#ff0088]`}>
+            onClick={() => { onFilterToggle(); onClick() }}
+            className={`${filterOn ? 'bg-[#ff0088]' : 'bg-zinc-900'} inline-block px-4 text-xs w-fit py-1 m-1 rounded-full border-2 border-zinc-900  text-zinc-50 hover:border-[#ff0088]`}>
             {text}
         </button>
 
